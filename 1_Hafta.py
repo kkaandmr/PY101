8a795d5b2e1dac7a1cbca9af6834043cea6a814b
kkaandmr <kkd1905@gmail.com>
2022-12-14 17:40:00 +0300

    PY101 1.Hafta

---
+++ 1_Hafta.py
@@ -0,0 +1,18 @@
+print("Hello World")
+ad="Kerem Kaan"
+soyad= "Demir"
+print(f"Ad : {ad} Soyad : {soyad}")
+print("Ad : {ad} Soyad : {soyad}")
+print("Hello %s %s" %(ad,soyad))
+
+x=int(input("1.Sayı : "))
+y=int(input("2.sayı : "))
+z=(x*y)
+t=(x/y)
+u=(x+y)
+v=(x-y)
+print(z)
+print(t)
+print(u)
+print(v)
+
